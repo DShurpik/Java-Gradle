9fcfae7d3062f3205b343e78db319ef7bfb0a34c
DShurpik <dimashurpik@gmail.com>
2025-04-19 16:24:38 +0200

    Refactor login functionality to use builder pattern and add fluent interface for login actions

--- src/test/java/sauceDemoTests.java
+++ src/test/java/sauceDemoTests.java
@@ -1,4 +1,5 @@
 import basePages.BaseTest;
+import dto.User;
 import io.qameta.allure.Owner;
 import io.qameta.allure.Severity;
 import io.qameta.allure.SeverityLevel;
@@ -7,6 +8,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import sauceDemo.LoginPage;
+import sauceDemo.LoginPageChain;
 import sauceDemo.MainPage;
 import utils.PropertyReader;
 
@@ -27,6 +29,45 @@ public class sauceDemoTests extends BaseTest {
         loginPage.enterUserName(PropertyReader.getRequiredProperty("userName"));
         loginPage.enterPassword(PropertyReader.getRequiredProperty("password"));
         loginPage.clickLoginButton();
+
+        assertTrue(mainPage.isMainPageDisplayed(), "Main page is not displayed");
+    }
+
+    @DisplayName("Login Test with builder")
+    @Severity(SeverityLevel.NORMAL)
+    @Owner("John Doe")
+    @TmsLink("https://example.com/testcase/2")
+    @Tag("smoke")
+    @Test
+    public void secondTest() {
+        LoginPage loginPage = new LoginPage();
+        MainPage mainPage = new MainPage();
+
+        User user = new User.UserBuilder()
+                .setUsername(PropertyReader.getRequiredProperty("userName"))
+                .setPassword(PropertyReader.getRequiredProperty("password"))
+                .build();
+        loginPage.open(PropertyReader.getRequiredProperty("baseUrl"));
+        loginPage.fillLoginForm(user);
+
+        assertTrue(mainPage.isMainPageDisplayed(), "Main page is not displayed");
+    }
+
+    @DisplayName("Login Test with chain")
+    @Severity(SeverityLevel.NORMAL)
+    @Owner("John Doe")
+    @TmsLink("https://example.com/testcase/3")
+    @Tag("smoke")
+    @Test
+    public void thirdTest() {
+        LoginPageChain loginPage = new LoginPageChain();
+        MainPage mainPage = new MainPage();
+
+        loginPage
+                .openUrl(PropertyReader.getRequiredProperty("baseUrl"))
+                .enterUserName(PropertyReader.getRequiredProperty("userName"))
+                .enterPassword(PropertyReader.getRequiredProperty("password"))
+                .clickLoginButton();
 
         assertTrue(mainPage.isMainPageDisplayed(), "Main page is not displayed");
     }
